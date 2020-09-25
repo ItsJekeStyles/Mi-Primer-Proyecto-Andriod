183c3dfd0562e3bb0182bf19a9630a807185c61e
ItsJekeStyles <70526943+ItsJekeStyles@users.noreply.github.com>
2020-09-24 19:50:42 -0500

    Version 1

---
+++ app/src/main/java/com/sodafres/holamundo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sodafres.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      button = (Button) findViewById(R.id.button);
+
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+              Toast.makeText(getApplicationContext(), R.string.Bienvenido, Toast.LENGTH_SHORT).show();
+          }
+      });
+    }
+}
